85a4b96a3c0882085ab6f5430bb8dc4559157bbf
Danil Chernikov <danil.chernikov@onix-systems.com>
2021-05-06 10:50:16 +0300

    Initial Commit

---
+++ Boosters iOS Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Boosters iOS Challenge
+//
+//  Created by Danil Chernikov on 06.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
